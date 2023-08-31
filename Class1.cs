6838002753550c32f243daaf9e4f5f911d86172f
Abhibhardiya24 <abhishekbhardiya1995@gmail.com>
2023-08-31 18:18:06 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,25 @@
+﻿namespace Udemy_Exercises
+{
+ 
+    
+        class area
+
+        {
+
+            static void main()
+
+            {
+
+                double p = 3.14159;
+
+                int r = 2;
+
+            double area = p * r * r;
+            
+             Console.WriteLine(area);
+             Console.ReadKey();
+
+            }
+
+        }
+    }
---
+++ Class2.cs
@@ -0,0 +1,47 @@
+﻿namespace Udemy_Exercises
+{
+    class Height
+
+
+
+    {
+
+
+
+        static void main()
+
+
+
+        {
+
+
+
+            int feet = 5;
+
+
+
+            int inch = 7;
+
+
+
+            int total_Inch = (feet * 12) + inch;
+
+
+
+            double total_Centimeter = total_Inch * 2.54;
+
+
+
+            Console.WriteLine(total_Centimeter);
+
+
+
+            Console.ReadKey();
+
+
+
+        }
+
+    }
+
+}
